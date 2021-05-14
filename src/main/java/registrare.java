9085c1a0fa7098afacd3923ffe3e56f9ec6d743a
BogoAdi <ovidiubogosel@yahoo.com>
2021-05-14 22:39:55 +0300

    Fisier FXML si Controler pentru Inregistrare

---
+++ src/main/java/registrare.java
@@ -0,0 +1,35 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+public class registrare {
+    @FXML
+    private Text registrationMessage;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private ChoiceBox role;
+
+    @FXML
+    public void initialize() {
+        role.getItems().addAll("Client", "Manager");
+    }
+
+    @FXML
+    public void handleRegisterAction() {
+        try {
+            UserService.addUser(usernameField.getText(), passwordField.getText(), (String) role.getValue());
+            registrationMessage.setText("Account created successfully!");
+        } catch (UsernameAlreadyExists e) {
+            registrationMessage.setText(e.getMessage());
+        }
+    }
+
+    public void handleBackAction() throws Exception{
+
+}
+}
