98dcceaf4258d8ac9ae418235d8becd1572ddc71
BogoAdi <ovidiubogosel@yahoo.com>
2021-05-15 08:12:37 +0300

    Adaugare meniuControler si legare a FXML-ului de el

---
+++ src/main/java/meniuManager.java
@@ -0,0 +1,40 @@
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class meniuManager {
+
+    private Stage window1;
+    private Scene scene;
+    private Parent root;
+
+
+    @FXML
+    private Button backButton;
+    @FXML
+    private Button viewButton;
+    @FXML
+    private Button editButton;
+
+
+    public void ViewOrders() throws Exception {
+
+    }
+
+
+    public void goToEditPage()throws  Exception {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("fxml/editPage.fxml"));
+        window1 = (Stage)editButton.getScene().getWindow();
+        window1.setScene(new Scene(root));
+
+
+
+    }
+
+    public void goBack() throws  Exception {
+
+    }
+}
