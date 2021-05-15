c907d4706d32919df8a8353410ca3d468b4326ec
ioanabalet <ale_11_ioana@yahoo.com>
2021-05-15 18:45:31 +0300

    realizare controler si fxml pentru vizualizare istoric comenzi

---
+++ src/main/java/VizIstoricComenzi.java
@@ -0,0 +1,69 @@
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+
+public class VizIstoricComenzi {
+
+
+        private Stage window1;
+        private Scene scene;
+        private Parent root;
+        @FXML
+        private TableView<Comanda> comenziTabel;
+        @FXML
+        private TableColumn<Comanda, String> produsColoana;
+        @FXML
+        private TableColumn<Produs, String> lemnColoana;
+
+        @FXML
+        private TableColumn<Produs, Integer> pretColoana;
+        @FXML
+        private TableColumn<Comanda, String> orasColoana;
+        @FXML
+        private TableColumn<Comanda, String> stradaColoana;
+        @FXML
+        private TableColumn<Comanda, String> numarColoana;
+
+        @FXML
+        private TableColumn<Comanda, Boolean>  asistentaColoana;
+        @FXML
+        private TableColumn<Comanda, String> Data;
+
+
+        @FXML
+        private Button backButton;
+
+        @FXML
+        private Label label1;
+
+
+        public void goBack()throws Exception {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("fxml/MeniuClient.fxml"));
+        window1 = (Stage)backButton.getScene().getWindow();
+        window1.setScene(new Scene(root));
+    }
+        ObservableList<Comanda> comenzi = FXCollections.observableArrayList(ComandaService.comenzi(User.getCurrentUser()));
+
+
+        public void initialize() {
+
+        produsColoana.setCellValueFactory(new PropertyValueFactory<>("Mobila"));
+        lemnColoana.setCellValueFactory(new PropertyValueFactory<>("Lemn"));
+        orasColoana.setCellValueFactory(new PropertyValueFactory<>("oras"));
+        pretColoana.setCellValueFactory(new PropertyValueFactory<>("pret"));
+        stradaColoana.setCellValueFactory(new PropertyValueFactory<>("strada"));
+        numarColoana.setCellValueFactory(new PropertyValueFactory<>("numarCasa"));
+        asistentaColoana.setCellValueFactory(new PropertyValueFactory<>("asistenta"));
+        Data.setCellValueFactory(new PropertyValueFactory<>("Data"));
+        comenziTabel.setItems(comenzi);
+
+
+    }
+}
